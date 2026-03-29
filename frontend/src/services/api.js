3c40020ed8c9140bdacf44f5dd074bb83a97ab24
Mr-Zenn <jayprakash6231ssb@gmail.com>
2026-03-29 09:39:44 +0530

    debug: check API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,11 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+// });
 import axios from "axios";
+
+console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
